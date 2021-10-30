f366f84b3b5a9bed1b774e76c37f01f3bf364953
Daniel Obot <danobot@hotmail.com>
2021-10-30 17:08:38 -0500

    chore: initial commit of the project

---
+++ src/main/java/com/herokuapp/obotapi/ObotApiApplication.java
@@ -0,0 +1,13 @@
+package com.herokuapp.obotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObotApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herokuapp/obotapi/ObotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herokuapp.obotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
