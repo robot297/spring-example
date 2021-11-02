a1b69afdc5bd9c7b3929957d29e4b706698bbfc1
Daniel Obot <danobot@hotmail.com>
2021-11-01 19:08:31 -0500

    feat: Expanded movie service interface to get all movies.

--- src/main/java/com/herokuapp/obotapi/service/MovieService.java
+++ src/main/java/com/herokuapp/obotapi/service/MovieService.java
@@ -2,7 +2,11 @@ package com.herokuapp.obotapi.service;
 
 import com.herokuapp.obotapi.dto.MovieDto;
 
+import java.util.List;
+
 public interface MovieService {
 
     MovieDto createMovie(MovieDto movieDto) throws Exception;
+
+    List<MovieDto> getMovies() throws Exception;
 }
