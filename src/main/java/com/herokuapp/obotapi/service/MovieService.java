ece053dd7f7dbece3df990c7cfa8eea52c0c5679
Daniel Obot <danobot@hotmail.com>
2021-11-04 17:04:27 -0500

    feat: MovieService interface now throws MovieExists exception

--- src/main/java/com/herokuapp/obotapi/service/MovieService.java
+++ src/main/java/com/herokuapp/obotapi/service/MovieService.java
@@ -1,12 +1,13 @@
 package com.herokuapp.obotapi.service;
 
 import com.herokuapp.obotapi.dto.MovieDto;
+import com.herokuapp.obotapi.exceptions.MovieExistsException;
 
 import java.util.List;
 
 public interface MovieService {
 
-    MovieDto createMovie(MovieDto movieDto) throws Exception;
+    MovieDto createMovie(MovieDto movieDto) throws MovieExistsException;
 
     List<MovieDto> getMovies() throws Exception;
 }
