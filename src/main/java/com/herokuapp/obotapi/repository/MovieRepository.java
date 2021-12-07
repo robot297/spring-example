67ec9bd7d003dbe1185f2e1e5b54655a4dea466e
Daniel Obot <danobot@hotmail.com>
2021-12-07 14:49:40 -0600

    feat: Updated spring repo to find by movie ID

--- src/main/java/com/herokuapp/obotapi/repository/MovieRepository.java
+++ src/main/java/com/herokuapp/obotapi/repository/MovieRepository.java
@@ -8,4 +8,7 @@ import org.springframework.stereotype.Repository;
 public interface MovieRepository extends JpaRepository<Movie, Long> {
     //This will find a movie in the repository by movie name.
     Movie findMovieByMovieName(String movieName);
+
+    // This will find a movie by its unique id.
+    Movie findMovieById(Long movieID);
 }
